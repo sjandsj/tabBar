3589afeea3f6ea9e0a477bf50f6110219c9e7efa
Shubhanshu Jain <sjain@gammastack.com>
2019-07-02 16:12:14 +0530

    Files Added

---
+++ tabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tabBar
+//
+//  Created by mac on 02/07/19.
+//  Copyright © 2019 gammastack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
